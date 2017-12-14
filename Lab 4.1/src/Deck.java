2059dbae6c0567e535ab9c9272f1e6c20a8221d4
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-12-14 13:15:42 -0500

    du

--- Lab 4.1/src/Deck.java
+++ Lab 4.1/src/Deck.java
@@ -2,12 +2,22 @@ import java.util.ArrayList;
 public class Deck {
 	private ArrayList<Card> unDealt;
 	private ArrayList<Card> Dealt;
-	public Deck() {
-		
+	public void DeckCreator(String[] rank, String[] suit, int[] pointvalue) {
+		for (int i = 0 ; i < rank.length;i++)
+		{
+			for (int j = 0; j < suit.length;j++)
+			{
+				for (int z = 0; z < pointvalue.length;z++)
+				{
+					Card newdeck = new Card(rank[i], suit[j], pointvalue[z]);
+					unDealt.add(newdeck);
+				}
+			}
+		}
 	}
 	public boolean isEmpty()
 	{
-		if(Card.size() == 0)
+		if(unDealt.size() == 0)
 		{
 			return true;
 		}
